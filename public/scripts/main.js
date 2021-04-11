dae71e808a9b60ab43a4fe97efe7ea235a467ce5
Andrei Gatej <andreigtj01@gmail.com>
2021-04-11 13:13:53 +0300

    feat(task7): add informational banner

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,7 @@ const $$ = document.querySelectorAll.bind(document);
 window.addEventListener('load', () => {
   allowTransitionsAfterLoad();
   activateHamburgerListener();
+  activateBannerAnimation();
 });
 
 function allowTransitionsAfterLoad () {
@@ -20,4 +21,23 @@ function activateHamburgerListener () {
   hamburger.addEventListener('click', () => {
     navContainer.classList.toggle('is-shown');
   });
+}
+
+function activateBannerAnimation () {
+  const banner = $('.js-dull-banner');
+
+  if (getComputedStyle(banner).display === 'none') {
+    return;
+  }
+
+  const activeClassName = 'is-active';
+  banner.classList.add(activeClassName);
+
+  const handler = ev => {
+    setTimeout(() => banner.classList.remove(activeClassName), 5000);
+
+    banner.removeEventListener('animationend', handler);
+  }
+
+  banner.addEventListener('animationend', handler);
 }
