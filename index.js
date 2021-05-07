aef01fa503a3edc6908b5f88cf12231e708c396f
Andrei Gatej <andreigtj01@gmail.com>
2021-05-07 23:38:43 +0300

    refactor: convert file from `.html` to `.ejs` while using partials

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import path from 'path';
 
-const PORT = 3002;
+const PORT = 8080;
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('public', 'views'));
+
 app.use(express.static('public'));
+app.use(express.static('public/assets'));
+
+app.get(/^\/(index)?$/, (req, res) => {
+  res.render('pages/index')
+});
+
+app.get(/^\/(about|hire\-a\-lawyer)$/, (req, res) => {
+  const { 0: pageName } = req.params;
+  
+  res.render(`pages/${pageName}`);
+});
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
