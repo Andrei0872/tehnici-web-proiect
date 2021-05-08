04c0fd37d96b33bdc0026a7c0483bb4ffff4f3ca
Andrei Gatej <andreigtj01@gmail.com>
2021-05-08 10:50:11 +0300

    feat: show `403` when accessing the gallery file

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from 'express';
 import path from 'path';
 
+/**
+ * @typedef {Object} CustomError
+ * @property {string} message - The error message
+ * @property {number} code - The error code
+ */
+
 const PORT = 8080;
+const GALLERY_FILE_NAME = 'gallery.json';
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('public', 'views'));
+
+app.use((req, res, next) => {
+  const err = req.path.includes(GALLERY_FILE_NAME) 
+    ? { message: 'Forbidden', code: 403 }
+    : undefined;
+
+  next(err);
+});
 
 app.use(express.static('public'));
 app.use(express.static('public/assets'));
@@ -24,6 +40,21 @@ app.get(/^\/(about|hire\-a\-lawyer)$/, (req, res) => {
 
 app.get('*', (req, res) => {
   res.render('pages/404');
+});
+
+app.use((/** @type {CustomError} */err, req, res, next) => {
+  let renderedPage;
+
+  switch (true) {
+    case err.code === 403: {
+      renderedPage = '403';
+      break;
+    }
+
+    // Other cases here...
+  }
+  
+  res.render(`pages/${renderedPage}`, { message: err.message });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
