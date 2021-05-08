32b71ef19266ec51a892e2ca337549d13b93dc62
Andrei Gatej <andreigtj01@gmail.com>
2021-05-08 17:29:44 +0300

    feat: automatically generate smaller images for the gallery

--- utils.js
+++ utils.js
@@ -33,3 +33,21 @@ export const resizeGalleryImages = async (widthForEachImage, imgDirectory, onImg
   console.log('All images have been resized successfully');
 };
 
+export const existsFileBasedOnCondition = async (directory, conditionFn) => {
+  const records = await readDirectory(directory);
+
+  return !!records.find(conditionFn);
+};
+
+export const checkSmallerImagesAreGenerated = async directory => {
+  // Generate smaller images on the fly(doing it only once)
+  const areFilesAlreadyGenerated = await existsFileBasedOnCondition(directory, fileName => /\-min/.exec(fileName));
+  if (!areFilesAlreadyGenerated) {
+    resizeGalleryImages(300, directory, ({ newImgPath }) => {
+      const lastDotPos = newImgPath.lastIndexOf('.');
+      const smallerImagePath = (newImgPath.slice(0, lastDotPos) + '-min.' + newImgPath.slice(lastDotPos + 1)).replace('temp-', '');
+
+      rename(newImgPath, smallerImagePath);
+    })
+  }
+};
