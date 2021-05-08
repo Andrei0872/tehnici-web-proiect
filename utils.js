35fc850faf027043296686864efed1266d94b880
Andrei Gatej <andreigtj01@gmail.com>
2021-05-08 17:30:04 +0300

    feat: fetch gallery images based on current season

--- utils.js
+++ utils.js
@@ -39,6 +39,37 @@ export const existsFileBasedOnCondition = async (directory, conditionFn) => {
   return !!records.find(conditionFn);
 };
 
+export const getCurrentSeason = () => {
+  const monthsSeasonsTable = [
+    { months: [12, 1, 2], season: 'winter', },
+    { months: [3, 4, 5], season: 'spring', },
+    { months: [6, 7, 8], season: 'summer', },
+    { months: [9, 10, 11], season: 'fall', },
+  ];
+  
+  const now = new Date();
+  const month = now.getMonth() + 1;
+
+  return monthsSeasonsTable.find(({ months }) => months.includes(month)).season;
+}
+
+export const fetchGalleryImages = async directory => {
+  const galleryMetadata = JSON.parse(await readFile(directory, 'utf8'));
+  const crtSeason = getCurrentSeason();
+
+  const images = galleryMetadata.images
+    // Filter out based on season
+    .filter(({ season }) => season === crtSeason)
+    .map(img => ({
+      ...img,
+      filePath: path.join(galleryMetadata.galleryPath, img.filePath),
+      filePathSmallerImg: path.join(galleryMetadata.galleryPath, img.filePathSmallerImg),
+    }));
+  
+  // Maximum 10 images
+  return images.length > 10 ? images.slice(0, 10) : images;
+};
+
 export const checkSmallerImagesAreGenerated = async directory => {
   // Generate smaller images on the fly(doing it only once)
   const areFilesAlreadyGenerated = await existsFileBasedOnCondition(directory, fileName => /\-min/.exec(fileName));
