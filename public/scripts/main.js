bd8f9b17818be018c3ceca49278dee80fc8bd3a8
Andrei Gatej <andreigtj01@gmail.com>
2021-05-09 15:23:54 +0300

    feat: add dynamic gallery

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,13 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const IMAGES_URL = 'http://localhost:8080/api/images';
+
 window.addEventListener('load', () => {
   allowTransitionsAfterLoad();
   activateHamburgerListener();
   activateBannerAnimation();
+  activateDynamicGallery();
 });
 
 function allowTransitionsAfterLoad () {
@@ -44,4 +47,62 @@ function activateBannerAnimation () {
   }
 
   banner.addEventListener('animationend', handler);
+}
+
+async function activateDynamicGallery () {
+  const getRandomBetween = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+  const getRandomNrOfImages = async () => {
+    const { images } = await fetch(IMAGES_URL).then(r => r.json());
+
+    images.sort(() => Math.random() > 0.5 ? -1 : 1);
+
+    let nrElements = getRandomBetween(5, 11);
+    nrElements = nrElements % 2 === 0 ? nrElements + 1 : nrElements;
+
+    return images.slice(0, nrElements);
+  }
+
+  const IMG_CLASS = 'dynamic-gallery__image';
+  const ACTIVE_CLASS = 'is-active';
+  const ANIMATION_DURATION_SECONDS = 2;
+
+  const galleryContainer = $('.js-dynamic-gallery');
+
+  const images = await getRandomNrOfImages();
+  const imagesCount = images.length;
+
+  // This is to prevent a white screen after the last image
+  images.push(images[0]);
+
+  const fragment = new DocumentFragment();
+
+  images.forEach((img, imgIdx) => {
+    const imgTag = document.createElement('img');
+    imgTag.classList.add(IMG_CLASS);
+    imgTag.src = img.filePath;
+
+    imgTag.style.cssText = `
+      animation-delay: ${imgIdx * ANIMATION_DURATION_SECONDS}s;
+      z-index: ${-1 * imgIdx};
+    `;
+
+    fragment.appendChild(imgTag);
+  });
+
+  galleryContainer.classList.add(ACTIVE_CLASS);
+  galleryContainer.appendChild(fragment);
+
+  const forceReflow = () => document.body.offsetHeight;
+
+  let endedTimes = imagesCount;
+  galleryContainer.addEventListener('animationend', () => {
+    if (--endedTimes === 0) {
+      endedTimes = imagesCount;
+
+      galleryContainer.classList.remove(ACTIVE_CLASS);
+      forceReflow();
+      galleryContainer.classList.add(ACTIVE_CLASS);
+    }
+  });
 }
