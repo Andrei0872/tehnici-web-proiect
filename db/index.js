b3b3a7c1b3ad9c68e22e93275c6c4964106f877e
Andrei Gatej <andreigtj01@gmail.com>
2021-06-26 14:21:58 +0300

    chore: add Heroku setup

--- db/index.js
+++ db/index.js
@@ -1,13 +1,18 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-const connConfig = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT || 5432,
-};
+const REMOTE_DB_URL = process.env.REMOTE_DB_URL || process.env.DATABASE_URL;
+const isRemoteDBUrlUsed = !!REMOTE_DB_URL;
+
+const connConfig = !isRemoteDBUrlUsed 
+  ? {
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT || 5432,
+  }
+  : { connectionString: REMOTE_DB_URL, ssl: { rejectUnauthorized: false } };
 
 export const init = async () => {
   const pool = new Pool(connConfig);
